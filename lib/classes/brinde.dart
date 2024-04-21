f601145c5aa21c4f5996cbf17ec0e930ba8e2422
Vinicius <viniciusrodriguesmuller@gmail.com>
2024-04-21 19:37:40 -0300

    feature:[R6-51]Cria classe Brinde e métodos para brinde

---
+++ lib/classes/brinde.dart
@@ -0,0 +1,24 @@
+class Brinde{
+  final String nome;
+  final int pontosNecessarios;
+  int qtdEmEstoque;
+
+  Brinde({
+    required this.nome, 
+    required this.pontosNecessarios, 
+    required this.qtdEmEstoque
+  });
+
+  void CalcularPontosNecessarios(){
+    print('Para ganhar um(a) ${this.nome} você precisa de ${this.pontosNecessarios} pontos.');
+  }
+
+  void RealizarTroca(){
+    if(this.qtdEmEstoque > 0){
+      this.qtdEmEstoque--;
+      print('Troca realizada! Você ganhou um(a) ${this.nome}.');
+    } else {
+      throw Exception('No momento não possuímos o produto ${this.nome} em estoque.');
+    }
+  }
+}
